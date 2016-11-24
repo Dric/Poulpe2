d4f592dabf60fc24999d65d40a7339abfcff943c
cedric.gallard <cedric.gallard@ch-george-sand.fr>
2016-11-24 16:56:46 +0100

    Correction d'un bug dans l'usage de fping dans la classe Check (encore)

--- classes/Check.php
+++ classes/Check.php
@@ -119,13 +119,14 @@ class Check {
 	public static function isOnline($server){
 		//exec('ping -c 1 -W 5 '.$server, $out);
 		// Plus rapide que le ping
-		exec('fping -c 1 -q '.$server, $out);
+		exec('fping '.$server.' 2>&1', $out);
 		if (!empty($out)){
 			// 172.32.1.125 : xmt/rcv/%loss = 1/1/0%, min/avg/max = 0.48/0.48/0.48
 			// 172.32.1.124 : xmt/rcv/%loss = 1/0/100%
 			// 172.32.1.124 is unreachable
+			//srv-tina : xmt/rcv/%loss = 1/1/0%, min/avg/max = 0.17/0.17/0.17
 			preg_match('/\d\/(\d)\/(\d{1,3})\%/i', $out[0], $matches);
-			if ((int)$matches[1] === 1) return true;
+			if ($matches[1] == '1') return true;
 		}
 		return false;
 	}
